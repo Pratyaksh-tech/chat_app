84d950b2cfa0cf2c2b664f8a5944e8d0c9cce05b
Pratyaksh-tech <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-04-29 14:55:53 +0530

    Delete App directory

--- App/models.py
+++
@@ -1,8 +0,0 @@
-from App import db, app
-
-
-class UserModel(db.Model):
-	id = db.Column(db.Integer(), primary_key=True);
-	e_mail = db.Column(db.String(length=20), nullable=False);
-	password = db.Column(db.String(length=20), nullable=False);
-
